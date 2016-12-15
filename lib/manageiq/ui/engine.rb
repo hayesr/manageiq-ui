73610e7bbe5b10f012f0863079d4a6254c800c4a
Eric Hayes <eric@erichayes.net>
2016-12-15 11:09:03 -0800

    These lines can be shorter.

--- lib/manageiq/ui/engine.rb
+++ lib/manageiq/ui/engine.rb
@@ -3,8 +3,8 @@ require 'patternfly-sass'
 module Manageiq
   module Ui
     class Engine < ::Rails::Engine
-      config.autoload_paths << File.expand_path(File.join(root, 'app', 'controllers', 'mixins'), __FILE__)
-      config.autoload_paths << File.expand_path(File.join(root, 'lib'), __FILE__)
+      config.autoload_paths << root.join('app', 'controllers', 'mixins')
+      config.autoload_paths << root.join('lib')
     end
   end
 end
